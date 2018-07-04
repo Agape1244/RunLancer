2d51cad202487328d8505a2b2375bb3c964ab048
Agape1244 <40817943+Agape1244@users.noreply.github.com>
2018-07-04 19:53:28 +0900

    準備
    構成の準備段階
    中身はカラ

--- Assets/Script/Blockmodel.cs
+++ Assets/Script/Blockmodel.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gamemaster : MonoBehaviour {
+public class Blockmodel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	//コメント追加してGitの練習
+	
 	// Update is called once per frame
 	void Update () {
 		
