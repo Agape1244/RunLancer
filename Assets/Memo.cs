356cc42b0a35be6ccd4cfd38af216897da2f44ff
Kazuki Suekawa <ahahasann02@gmail.com>
2018-07-07 02:08:59 +0900

    画像の追加、仮配置。 プレイヤーの重力と当たり判定。床の当たり判定を導入。 メモ多数記入。

---
+++ Assets/Memo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Memo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+//背景は２段か３段森タイプにする予定
+//画像お借り中
+//現在はブロックのモデルとビュー、プレイヤーのモデルとビューを別で管理する予定だが、一つでまとまりそう？
+//自分の中のMVCを一度試してみる。合ってるか確認する。
+//UIの仮配置をする。
