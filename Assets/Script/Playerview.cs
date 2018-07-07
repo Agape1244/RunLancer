aa963e41a24992973e39694987ff43f9d4c2862d
Kazuki Suekawa <ahahasann02@gmail.com>
2018-07-07 22:25:21 +0900

    プレイヤーにジャンプの追加。要修正。 UIの枠作成とTextの追加（ScoreとSpeed） Player等の位置の微調整

--- Assets/Script/Playerview.cs
+++ Assets/Script/Playerview.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Playerview : MonoBehaviour {
 
+    private Rigidbody2D rb2D;//リジットボデーの箱
+    private float jumpForce = 10.0f;//ジャンプパワー１０
+
 	// Use this for initialization
 	void Start () {
+        rb2D = GetComponent<Rigidbody2D>();//リジットボデーの読み込み
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0))
+        {
+            Jump();//←マウスクリックでジャンプ。命令内容は下で
+        }
 	}
+
+    void Jump()//ジャンプの命令内容
+    {
+        rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
+    }//ベロシティは早さをつかさどる（べくたー２の）
+    //これによりリジットボデーに指定されたオブジェクトはX軸は特になし、Y軸にジャンプフォースで指示したぱうわー１０の速度、で動く、という命令ジャンプができた。
+    //todo 動作確認完了。クリックでジャンプ。ふわふわしてた。２段３段とジャンプできちゃう。要修正
 }
