0f437981d8152cef5d7b891f7e4dcd1529b70ab8
Kazuki Suekawa <ahahasann02@gmail.com>
2018-07-08 11:58:23 +0900

    ブロックの生成、移動の追加 課題おおし

--- Assets/Script/Blockview.cs
+++ Assets/Script/Blockview.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class Blockview : MonoBehaviour {
 
+    public GameObject Rblock;
+    public GameObject Bblock;
+    public GameObject movingBlock;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(MoveMap());//このふたつはこるーちんしちゃうぜ
+        StartCoroutine(SetBlock());
 	}
 	
+    IEnumerator SetBlock()//ブロックをセットする命令
+    {
+        while (true)
+        {
+            Vector3 pos = new Vector3(11, Random.Range(3f, -1.5f), 0);
+            Vector3 pos1 = new Vector3(11, Random.Range(3f, -1.5f), 0);
+            GameObject block=Instantiate(Bblock,pos,transform.rotation) as GameObject;
+            GameObject block1 = Instantiate(Rblock, pos1, transform.rotation) as GameObject;
+            block.transform.parent = movingBlock.transform;
+            block1.transform.parent = movingBlock.transform;
+            yield return new WaitForSeconds(1.5f);
+            //todo　ほぼ借り物。解析まだ。実行可否まだ。つまりまだまだ。
+            //todo ぅえいとふぉあせこんどずの（）内はSpeedに合わせて間隔短くしていきたい
+        }
+     
+    }
+
+    IEnumerator MoveMap()//ブロックが移動する命令
+    {
+        while (true) {
+            Vector3 pos = movingBlock.transform.position;
+            pos.x -= 4 * Time.deltaTime;
+            movingBlock.transform.position = pos;
+            yield return 0;//todo これも借り物。解析まだ。背景動かすものっぽい？
+        }
+    }
 	// Update is called once per frame
 	void Update () {
 		
